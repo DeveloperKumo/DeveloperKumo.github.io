9ad644393a6a7a9ac803a55e365fe917c68b090a
Kumo <developerkumo@gmail.com>
2024-01-26 16:45:32 -0500

    Updated page locations for gofiber

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
-	app.Static("/", "./pages")
+	app.Static("/", "index.html")
 	app.Static("/misc", "./misc")
 
 	app.Get("/", func(c *fiber.Ctx) error {
