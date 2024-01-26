de7217f726cc597fee8cd20080ee2a4decc1aba0
Kumo <developerkumo@gmail.com>
2024-01-26 17:19:48 -0500

    updated index file to intereact with gofiber/HTMX

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ func main() {
 	app := fiber.New()
 
 	app.Static("/", "index.html")
+	app.Static("/", "./pages")
 	app.Static("/misc", "./misc")
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("420")
+		return c.SendString("error")
+	})
+
+	app.Get("/contact", func(c *fiber.Ctx) error {
+
+		return c.SendString("contact.html")
 	})
 
 	app.Listen(":6969")
